6ae1c004c7f0c6f99ea2d9bf72b2495b29c19de8
malik672 <aremumalik01@gmail.com>
2023-11-05 16:53:58 +0100

    add priority fee

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use ethers::prelude::*;
-// use ethers::core::types::Block;
-use ethers::types::U256;
+use num::bigint::BigUint;
+use num::traits::FromPrimitive;
+use primitive_types::U256;
 use reqwest::Client;
 use serde_json;
 use serde_json::json;
 use std::collections::HashMap;
+use std::num::IntErrorKind::PosOverflow;
 
 #[derive(Debug)]
 enum EthRpcMethod {
@@ -42,19 +43,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         fetch_ethereum_data(&EthRpcMethod::EthBlockNumber, ethereum_rpc_url).await?;
     println!("Block Number: {}", gas_block_number);
 
-    // Fetch block number
-    // let block_number_hex =
-    //     fetch_ethereum_data(&EthRpcMethod::EthBlockNumber, ethereum_rpc_url).await?;
-    // let get_block =
-    //     fetch_ethereum_data(&EthRpcMethod::EthGetBlockByNumber, ethereum_rpc_url).await?;
-
+    //fetch block info using block number
     let fetch_block = fetch_block_by_number(
         &gas_block_number,
         &EthRpcMethod::EthGetBlockByNumber,
         ethereum_rpc_url,
     )
     .await?;
-    println!("Current Gas Price (in Gwei): {}", fetch_block);
+
+    let gas_price_wei = hex_to_decimal(&gas_price_wei);
+    
+    //parse_hex_to_u256(&fetch_block).unwrap()
+
+//    let gas: HashMap<String, serde_json::Value> = gas_calculator(&fetch_block);
 
     Ok(())
 }
@@ -106,11 +107,45 @@ async fn fetch_block_by_number(
     Ok(base_fee_per_gas.to_string())
 }
 
+
+// fn gas_calculator(base_fee: &str) -> HashMap<String, serde_json::Value> {
+   
+// }
+
+fn calculate_priority_fees(standard_gas_price: f64) -> (f64, f64, f64) {
+    // Define the percentage differences for slow and fast options
+    let slow_percentage = 0.8;  // 80% of the standard gas price
+    let fast_percentage = 1.2;  // 120% of the standard gas price
+
+    // Calculate the slow, standard, and fast priority fees
+    let slow_fee = standard_gas_price * slow_percentage;
+    let standard_fee = standard_gas_price;
+    let fast_fee = standard_gas_price * fast_percentage;
+
+    (slow_fee, standard_fee, fast_fee)
+}
+
 fn convert_wei_to_gwei(wei_value: &str) -> f64 {
     let wei_value = wei_value.parse::<f64>().unwrap();
     wei_value / 1_000_000_000.0
 }
 
-fn parse_hex_to_u64(hex_value: &str) -> u64 {
-    u64::from_str_radix(&hex_value[2..], 16).unwrap()
+fn parse_hex_to_u256(hex: &str) -> Option<BigUint> {
+    let hex = hex.replace("\"", "");
+
+    // Remove the "0x" prefix if present
+    let hex = hex.trim_start_matches("0x");
+
+    // Parse the hexadecimal string into a BigUint
+    let big_uint = <BigUint as num::Num>::from_str_radix(hex, 16);
+
+    // Return the result
+    big_uint.ok()
+}
+
+fn hex_to_decimal(hex: &str) -> u64 {
+    // Remove the optional "0x" prefix from the hex string
+    let hex = hex.trim_start_matches("0x");
+    // Parse the hex string as a u64 with radix 16
+    u64::from_str_radix(hex, 16).unwrap()
 }
